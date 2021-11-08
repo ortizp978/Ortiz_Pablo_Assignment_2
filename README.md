af31b8c2ac854b974f73082498cc957b1f163069
Pablo Ortiz <pablortiz78@MacBook-Pro-de-Pablo.local>
2021-11-08 18:43:14 -0500

    New Update

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
 # New Game by Pablo Ortiz!
 
-This is a new game based on Paper, Scissors and Rock learned at class
+This is a new game based on Paper, Scissors and Rock learned at class.
 
-## Contributing
+Inspired by a similar game that my son currently plays with his friends where he basically uses the same mechanics but with different options or "weapons".
 
-1. Fork it!
-2. Create your feature branch: `git checkout -b my-new-feature`
-3. Commit your changes: `git commit -am 'Add some feature'`
-4. Push to the branch: `git push origin my-new-feature`
-5. Submit a pull request :D
+In this new case Rock is replaced for Rabbit.
+Paper is replaced for Gun.
+Finally Scissors is replaced for Wall.
+
+So ... Rabbit is killed by a Gun, Gun is crushed by a Wall and Wall is jumped by a Rabbit.
+
+Now we will try to make this game look better in the terminal window.
 
 
-## Credits
+Thanks to the professor Trevor for teaches this, for me have been a quite difficult but the important thing is try to improved.
+Thanks to my son to show me a new way to play this popular game.
 
-TODO: Write credits > list yourself, team members, anywhere you're getting references (code, visual references, etc)
-
-## License
-
-MIT
+Autor: Pablo Ortiz.
---
+++ main.py
@@ -0,0 +1,82 @@
+from random import randint
+from gameComponents import winLose
+
+choices = ["rabbit", "gun", "wall"]
+
+Dad= False
+
+# add player 
+DadLives = 5
+SonLives = 5
+
+print(" ")
+print("LETS PLAY WITH YOUR SON RABBIT - GUN - WALL!")
+print(" ")
+print("********* INSTRUCTIONS *********")
+print("Select between RABBIT, GUN & WALL.")
+print("WALL defeats GUN, GUN defeats RABBIT, and RABBIT defeats WALL.")
+print("Good Luck Daddy!")
+print(" ")
+
+while Dad is False:
+
+
+	Dad =input("Make your selection! :")
+	Son = choices[randint(0,2)]
+
+	print("Dad chose: " + Dad)
+	print("Son chose: " + Son)
+
+	#=====================================
+
+	if (Son == Dad):
+		print(" ")
+		print("You select the same! Choose again")
+
+	elif (Dad == "rabbit"):
+		if (Son == "gun"):
+			print(" ")
+			print("Dad lose!")
+			DadLives = DadLives - 1
+		else:
+			print(" ")
+			print("DAD WIN!")
+			SonLives = SonLives - 1
+
+	elif (Dad == "gun"):
+		if (Son == "wall"):
+			print(" ")
+			print("Dad lose!")
+			DadLives = DadLives - 1
+		else:
+			print(" ")
+			print("DAD WIN!")
+			SonLives = SonLives - 1
+
+	elif (Dad == "wall"):
+		if (Son == "rabbit"):
+			print(" ")
+			print("Dad lose!")
+			DadLives = SonLives - 1
+		else:
+			print(" ")
+			print("DAD WIN!")
+			SonLives = SonLives - 1
+
+	print("==============")
+	print("Dad Lives " + str(DadLives))
+	print("Son Lives: " + str(SonLives))
+	print("==============")
+	print(" ")
+
+	#=====================================
+
+	if DadLives == 0:
+		# call the winorlose function here
+		winLose.winorlose(" lost :( ")
+
+	elif SonLives == 0:
+		# call the winorlose function here
+		winLose.winorlose(" WON!! :) :) :) :) ")
+
+	Dad = False
